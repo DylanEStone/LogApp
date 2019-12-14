8c1c46bdbb2c3ee24163177e5bc95d086dda39a8
Dylan Stone <dylanevanstone@gmail.com>
2019-12-14 11:55:26 -0800

    working on database integration

--- app.js
+++ app.js
@@ -2,6 +2,22 @@
 var express = require('express');
 var path = require('path')
 var app = express();
+const { Sequelize, Model, DataTypes } = require('sequelize');
+
+
+class Entry extends Model {}
+Entry.init({
+    time: DataTypes.INTEGER,
+    dss: DataTypes.INTEGER,
+    sc: DataTypes.INTEGER,
+    type: DataTypes.STRING,
+    entry: DataTypes.STRING,
+})
+
+const sequelize = new Sequelize('dsotlog', 'postgres', 'a5ac6f3e8988', {
+    dialect: 'postgres',
+    storage: 'LocalHost'
+})
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
